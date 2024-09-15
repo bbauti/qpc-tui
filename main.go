adfb2b795f5b458921709dab7edbf3af50a4b30b
Bautista Igarzabal <bautista.igarzabal@avoristravel.com>
2024-09-15 16:23:21 -0300

    feat: init

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"qpc-tui/scraper"
+)
+
+const url = "https://quepensaschacabuco.com/"
+
+type model struct {
+	status      int
+	currentPage int
+	entries     []scraper.Article
+	canContinue bool
+	canGoBack   bool
+	err         error
+}
+
+type statusMsg int
+
+type errMsg struct{ err error }
+
+func (e errMsg) Error() string { return e.err.Error() }
+
+func checkServer() tea.Msg {
+	c := &http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(url)
+
+	if err != nil {
+		return errMsg{err}
+	}
+	return statusMsg(res.StatusCode)
+}
+
+func (m model) Init() tea.Cmd {
+	return checkServer
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case statusMsg:
+		m.status = int(msg)
+		if m.status == 200 {
+			entries, canContinue, canGoBack, err := scraper.ScrapePage(m.currentPage)
+			if err != nil {
+				fmt.Println("Error scraping page:", err)
+			} else {
+				m.entries = entries
+				m.canContinue = canContinue
+				m.canGoBack = canGoBack
+			}
+		}
+		return m, tea.Quit
+
+	case errMsg:
+		m.err = msg
+		return m, tea.Quit
+
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
+	}
+
+	s := fmt.Sprintf("Checking %s...\n\n", url)
+
+	if m.status > 0 {
+		s += fmt.Sprintf("Status: %d\n", m.status)
+		s += fmt.Sprintf("Can continue: %v\n", m.canContinue)
+		s += fmt.Sprintf("Can go back: %v\n", m.canGoBack)
+		s += fmt.Sprintf("Current page: %d\n", m.currentPage)
+		s += fmt.Sprintf("Entries: %d\n", len(m.entries))
+	} else {
+		s += "Loading..."
+	}
+
+	return "\n" + s + "\n\n"
+}
+
+func main() {
+	if _, err := tea.NewProgram(model{}).Run(); err != nil {
+		fmt.Printf("Uh oh, there was an error: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ scraper/scraper.go
@@ -0,0 +1,198 @@
+package scraper
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gocolly/colly/v2"
+)
+
+var (
+	spanishMonths = map[string]string{
+		"Enero":      "January",
+		"Febrero":    "February",
+		"Marzo":      "March",
+		"Abril":      "April",
+		"Mayo":       "May",
+		"Junio":      "June",
+		"Julio":      "July",
+		"Agosto":     "August",
+		"Septiembre": "September",
+		"Octubre":    "October",
+		"Noviembre":  "November",
+		"Diciembre":  "December",
+	}
+
+	spanishDays = map[string]string{
+		"Lunes":     "Monday",
+		"Martes":    "Tuesday",
+		"Miercoles": "Wednesday",
+		"Jueves":    "Thursday",
+		"Viernes":   "Friday",
+		"Sabado":    "Saturday",
+		"Domingo":   "Sunday",
+	}
+)
+
+type Article struct {
+	Title      string `json:"title"`
+	Date       string `json:"date"`
+	Category   string `json:"category"`
+	CategoryId int    `json:"category_id"`
+	Body       string `json:"body"`
+	Link       string `json:"link"`
+}
+
+func parseSpanishDate(dateStr string) (time.Time, error) {
+
+	for spanish, english := range spanishMonths {
+
+		dateStr = strings.ReplaceAll(dateStr, spanish, english)
+	}
+	for spanish, english := range spanishDays {
+		dateStr = strings.ReplaceAll(dateStr, spanish, english)
+	}
+
+	dateStr = strings.NewReplacer("de", "", ".", "").Replace(dateStr)
+	return time.Parse("Monday, 02 January 2006 15:04 Hs", strings.TrimSpace(dateStr))
+}
+
+func setupCollectors(c *colly.Collector, links *[]string, articles *[]Article, mu *sync.Mutex, wg *sync.WaitGroup, canContinue *bool, canGoBack *bool) {
+	setupMainCollector(c, links, ".categoria_8", canContinue, canGoBack)
+	contentCollector := setupContentCollector(c, articles, mu)
+	setupOnScrapedCallback(c, contentCollector, links, wg)
+}
+
+func setupMainCollector(c *colly.Collector, links *[]string, additionalClass string, canContinue *bool, canGoBack *bool) {
+	class := " .noticia1" + additionalClass
+
+	c.OnHTML("[data-link]"+class, func(e *colly.HTMLElement) {
+		parent := e.DOM.Parent()
+
+		if link := parent.AttrOr("data-link", ""); link != "" {
+			*links = append(*links, link)
+		}
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		if len(*links) == 0 {
+			log.Println("Error: No elements found with the specified selector.")
+		}
+	})
+
+	c.OnHTML(".pagination a", func(e *colly.HTMLElement) {
+		fmt.Println("Pagination:", e.Text)
+		if e.Text == "Siguiente" {
+			*canContinue = true
+		}
+		if e.Text == "Anterior" {
+			*canGoBack = true
+		}
+	})
+}
+
+func setupContentCollector(c *colly.Collector, articles *[]Article, mu *sync.Mutex) *colly.Collector {
+	contentCollector := c.Clone()
+
+	contentCollector.OnHTML(".noticia-detalle", func(e *colly.HTMLElement) {
+		article := parseArticle(e)
+
+		if article != nil {
+			mu.Lock()
+			*articles = append(*articles, *article)
+			mu.Unlock()
+		}
+	})
+	return contentCollector
+}
+
+func parseArticle(e *colly.HTMLElement) *Article {
+	info := e.ChildText(".noticia-detalle-info")
+	title := e.ChildText(".titulo2 ")
+	category := e.ChildText(".titulo")
+	classes := e.DOM.AttrOr("class", "")
+
+	re := regexp.MustCompile(`categoria_(\d+)`)
+	matches := re.FindStringSubmatch(classes)
+	if len(matches) < 2 {
+		log.Printf("Error: the categoryId was not found for the article: %s", title)
+		return nil
+	}
+
+	categoryId, err := strconv.Atoi(matches[1])
+	if err != nil {
+		log.Printf("Error parsing categoryId: %v", err)
+		return nil
+	}
+
+	body := strings.Join(e.ChildTexts("p"), "\n\n")
+
+	t, err := parseSpanishDate(info)
+	if err != nil {
+		log.Printf("Error parsing date: %v", err)
+		return nil
+	}
+
+	return &Article{
+		Title:      title,
+		Date:       t.Format("2006-01-02 15:04:05"),
+		Category:   category,
+		CategoryId: categoryId,
+		Body:       body,
+		Link:       e.Request.URL.String(),
+	}
+}
+
+func setupOnScrapedCallback(c *colly.Collector, contentCollector *colly.Collector, links *[]string, wg *sync.WaitGroup) {
+	c.OnScraped(func(r *colly.Response) {
+
+		for _, link := range *links {
+
+			wg.Add(1)
+
+			go func(url string) {
+
+				defer wg.Done()
+				if err := contentCollector.Visit(url); err != nil {
+					log.Printf("Error visiting %s: %v", url, err)
+				}
+			}(link)
+		}
+		*links = []string{}
+	})
+}
+
+func ScrapePage(page int) ([]Article, bool, bool, error) {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.quepensaschacabuco.com"),
+	)
+
+	var (
+		links       []string
+		articles    []Article
+		canContinue bool
+		canGoBack   bool
+
+		mu sync.Mutex
+
+		wg sync.WaitGroup
+	)
+
+	setupCollectors(c, &links, &articles, &mu, &wg, &canContinue, &canGoBack)
+
+	err := c.Visit(fmt.Sprintf("https://www.quepensaschacabuco.com/entradas/%d/", page))
+	if err != nil {
+		return nil, false, false, err
+	}
+
+	c.Wait()
+
+	wg.Wait()
+
+	return articles, canContinue, canGoBack, nil
+}
